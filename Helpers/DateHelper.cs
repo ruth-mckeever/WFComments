d4995077b9bcdb827d560a51bd6aff9d0f517fe9
Ruth McKeever <mckeevec@dkit.ie>
2024-11-13 15:42:00 +0000

    Week 9 changes - equals, compare to, sort, delete, title & timestamp.

---
+++ Helpers/DateHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace WFComments.Helpers
+{
+    public class DateHelper
+    {
+        public string FormatDate(DateTime timestamp)
+        {
+            DateTime today = DateTime.Today;
+            DateTime yesterday = DateTime.Today.AddDays(-1);
+            DateTime oneWeekAgo = DateTime.Today.AddDays(-7);
+
+            if (timestamp >= today)
+            {
+                return timestamp.ToShortTimeString();
+            }
+            else if (timestamp >= yesterday)
+            {
+                return $"Yesterday at {timestamp.ToString("h:mmtt")}";
+            }
+            else if (timestamp > oneWeekAgo)
+            {
+                return timestamp.ToString("ddd h:mmtt");
+            }
+
+            return timestamp.ToString("dd/MM/yyyy");
+        }
+    }
+}
--- UserComment.cs
+++ UserComment.cs
@@ -1,9 +1,12 @@
 ﻿namespace WFComments
 {
-    public class UserComment
+    public class UserComment : IComparable<UserComment>
     {
         private string rawComment;
         private string sanitizedComment;
+        private string rawTitle;
+        private string sanitizedTitle;
+        private DateTime createdTimestamp;
 
         public string RawComment
         {
@@ -17,10 +20,70 @@
             set { sanitizedComment = value; }
         }
 
-        public UserComment(string rawComment)
+        public string RawTitle
+        {
+            get { return rawTitle; }
+            set { rawTitle = value; }
+        }
+
+        public string SanitizedTitle
+        {
+            get { return sanitizedTitle; }
+            set { sanitizedTitle = value; }
+        }
+
+        public DateTime CreatedTimestamp
+        {
+            get { return createdTimestamp; }
+        }
+
+        public UserComment(string rawTitle, string rawComment)
         {
             this.rawComment = rawComment;
-            sanitizedComment = SanitizeService.SanitizeTextJSON(rawComment); //TODO: will call sanitize service here
+            sanitizedComment = SanitizeService.SanitizeTextJSON(rawComment);
+            this.rawTitle = rawTitle;
+            sanitizedTitle = SanitizeService.SanitizeTextJSON(rawTitle);
+            createdTimestamp = DateTime.Now;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            //We will update this whenever we change user comment
+            if (!(obj is UserComment))
+            {
+                return false;
+            }
+            var userComment = (UserComment)obj;
+            if (userComment.sanitizedComment.Equals(sanitizedComment) && userComment.sanitizedTitle.Equals(sanitizedTitle))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator ==(UserComment left, UserComment right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(UserComment left, UserComment right)
+        {
+            return !left.Equals(right);
+        }
+        public override int GetHashCode()
+        {
+            //We will update this whenever we change user comment
+            return (sanitizedTitle, sanitizedComment).GetHashCode();
+        }
+        public int CompareTo(UserComment? other)
+        {
+            int result = string.Compare(sanitizedTitle, other.SanitizedTitle, ignoreCase: true);
+            if (result == 0)
+            {
+                result = string.Compare(sanitizedComment, other.SanitizedComment, ignoreCase: true);
+            }
+            return result;
         }
     }
+
+
+
 }
